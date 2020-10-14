a23d587b985b0099e8e37f40068cf56eebc907f9
Cody Leng <codyleng@codys-macbook.home>
2020-10-14 16:15:35 +1100

    add background color

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { Inner, Container } from "./styles/jumbotron";
+import {
+  Inner,
+  Container,
+  SubTitle,
+  Title,
+  Pane,
+  Image,
+  Item,
+} from "./styles/jumbotron";
 
 export default function Jumbotron({
   children,
   direction = "row",
   ...restProps
 }) {
-  return <Inner direction={direction}>{children}</Inner>;
+  return (
+    <Item direction={direction}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
 }
 
 Jumbotron.Container = function JumbotronContainer({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronContainer({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
 };
